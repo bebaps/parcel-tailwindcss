29bf4fc3bde9ad39527d090144ce1e736a55d67c
Alexander Persky <info@alexanderpersky.com>
2019-12-07 10:16:46 -0700

    Add files

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.html', './src/images/**/*.svg', './src/js/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  // defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss')('tailwind.config.js'),
+    require('postcss-preset-env')({
+      stage: 1,
+      autoprefixer: {
+        grid: true
+      }
+    }),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ]
+}
